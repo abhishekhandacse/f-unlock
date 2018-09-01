ee26ac2c4f197cead722effbdfb18459467d0203
Abhishek Salwan <salwan221@Abhisheks-MacBook-Pro.local>
2018-09-01 11:06:27 +0530

    face comparison script added

---
+++ face_comparison.py
@@ -0,0 +1,29 @@
+#Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#PDX-License-Identifier: MIT-0 (For details, see https://github.com/awsdocs/amazon-rekognition-developer-guide/blob/master/LICENSE-SAMPLECODE.)
+
+import boto3
+
+if __name__ == "__main__":
+
+    sourceFile='1.png'
+    targetFile='209.png'
+    client=boto3.client('rekognition')
+   
+    imageSource=open(sourceFile,'rb')
+    imageTarget=open(targetFile,'rb')
+
+    response=client.compare_faces(SimilarityThreshold=70,
+                                  SourceImage={'Bytes': imageSource.read()},
+                                  TargetImage={'Bytes': imageTarget.read()})
+    
+    for faceMatch in response['FaceMatches']:
+        position = faceMatch['Face']['BoundingBox']
+        confidence = str(faceMatch['Face']['Confidence'])
+        print('The face at ' +
+               str(position['Left']) + ' ' +
+               str(position['Top']) +
+               ' matches with ' + confidence + '% confidence')
+    	print confidence
+
+    imageSource.close()
+    imageTarget.close()               
